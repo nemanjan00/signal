f7972db85a266b638d68da52ddabc5d4be23f8e7
nemanjan00 <nemanjan00@gmail.com>
2023-01-23 22:04:32 +0100

    Tone generator

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
+const FFT = require("fft.js");
+const WaveFile = require('wavefile').WaveFile;
+const fs = require("fs");
+
+const samplerate = 44100;
+const bitrate = 32;
+
+const tone = require("./tone")(samplerate, 2000, 0.7 * Math.pow(2, bitrate - 1));
+
+const secondTone = tone(1000);
+
+console.log(Math.max(...secondTone));
+
+let wav = new WaveFile();
+wav.fromScratch(1, samplerate, '32', secondTone);
+
+fs.writeFileSync("./test.wav", wav.toBuffer());
+
+const windowSize = 4096;
+
+const fft = new FFT(windowSize);
+
+const input = secondTone.slice(0, windowSize)
+
+const data = fft.toComplexArray(input);
+
+const out = new Array(windowSize);
+
+fft.realTransform(out, data)
+
 module.exports = {};
+
+//console.log(out);
 
--- src/tone/index.js
+++ src/tone/index.js
@@ -1,2 +1,13 @@
-module.exports = {};
+module.exports = (samplerate, freq, volume) => {
+	return (duration) => {
+		const resultSize = (duration / 1000) * samplerate;
 
+		const result = new Array(resultSize)
+			.fill(0)
+			.map((_, pos) => {
+				return volume * Math.sin(pos / samplerate * (2 * Math.PI) * freq);
+			});
+
+		return result;
+	};
+};
