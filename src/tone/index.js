8e86ed96cebda70b0d70db7d0a23a3ae5972bc26
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2023-01-25 11:51:04 +0100

    Fix tone for different baudrate

--- src/tone/index.js
+++ src/tone/index.js
@@ -2,7 +2,7 @@ module.exports = (samplerate, freq, volume) => {
 	return (duration) => {
 		const resultSize = (duration / 1000) * samplerate;
 
-		const result = new Array(resultSize)
+		const result = new Array(Math.round(resultSize))
 			.fill(0)
 			.map((_, pos) => {
 				return volume * Math.sin(pos / samplerate * (2 * Math.PI) * freq);
